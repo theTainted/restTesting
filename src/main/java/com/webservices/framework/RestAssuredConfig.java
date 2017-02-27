6485e7f3052ce26ce8d3ec165c068e90a6521817
syam.s@outlook.com <Liverp00l>
2017-02-27 12:47:17 +0100

    tried creating a config path

--- src/main/java/com/webservices/framework/RestAssuredConfig.java
+++ src/main/java/com/webservices/framework/RestAssuredConfig.java
@@ -1,7 +1,17 @@
 package com.webservices.framework;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeSuite;
+
 /**
  * Created by syam.suryanarayanan on 2/27/2017.
  */
 public class RestAssuredConfig {
+    @BeforeSuite(alwaysRun=true)
+
+    public void configure(){
+        RestAssured.baseURI="http://t2.mobile.fleurametz.com";
+
+        RestAssured.basePath= "mobile-api/v1/";
+    }
 }
