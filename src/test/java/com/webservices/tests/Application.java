791514cb29b3c843d9389ce6efe3ce8d048efd77
syam.s@outlook.com <Liverp00l>
2017-02-27 14:18:12 +0100

    doesnot work, tried using languageISO

--- src/test/java/com/webservices/tests/Application.java
+++ src/test/java/com/webservices/tests/Application.java
@@ -10,10 +10,10 @@ import static io.restassured.RestAssured.given;
  * Created by syam.suryanarayanan on 2/27/2017.
  */
 public class Application {
-    @Test
+ /*   @Test
     public void validateApplication() {
         given().get("http://t2.mobile.fleurametz.com/nl-NL/mobile-api/v1/application?contextId=4999").then().statusCode(200).log().all();
-    }
+    }*/
 
     /* @Test(groups="demo")
         public void validateApplication2(){
