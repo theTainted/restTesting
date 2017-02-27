6f22fe9f22e1c69a153fd9b9ebc7bee5b85ba845
syam.s@outlook.com <Liverp00l>
2017-02-27 12:36:19 +0100

    Added the first testclass called Application

---
+++ src/test/java/com/webservices/tests/Application.java
@@ -0,0 +1,17 @@
+package com.webservices.tests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * Created by syam.suryanarayanan on 2/27/2017.
+ */
+public class Application {
+@Test
+    public void validateApplication(){
+        given().get("http://t2.mobile.fleurametz.com/nl-NL/mobile-api/v1/application?contextId=4999").then().statusCode(200).log().all();
+    }
+
+
+}
